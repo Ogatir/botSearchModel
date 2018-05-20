cf3f28303adcfa0e6d91715e9bd9aa38dd6f2f4d
Ogatir <ivan-kar@mail.ru>
2018-05-20 14:48:25 +0300

    Started criteria settings

---
+++ src/main/java/UnifiedCriteria.java
@@ -0,0 +1,13 @@
+public class UnifiedCreteria {
+
+    private boolean hasFoto;
+    private boolean isVerified;
+    private boolean wallNotEmpty;
+    private boolean specialwallTiming;
+    private boolean repostDominateOnOriginal;
+
+
+    public UnifiedCreteria(){
+
+    }
+}
